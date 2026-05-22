c6410933c7364a57ece728fa4e8be045741dd079
Luis Pedro Mayorga Rodríguez <luispedrom2006@umes.edu.gt>
2026-05-21 19:58:44 -0600

    Se crean las carpetas Services y Models, en Models se crean las clases Album y Cancion

---
+++ Models/Album.cs
@@ -0,0 +1,11 @@
+﻿namespace LAB14_Ejer2Repaso.Models
+{
+    public class Album
+    {
+        public string TituloAlbum { get; set; } = string.Empty;
+        public string ArtistaAlbum { get; set; } = string.Empty;
+        public DateTime FechaPublicacion { get; set; } = DateTime.Now;
+
+        public List<Cancion> Canciones { get; set; } = new List<Cancion>();
+    }
+}
---
+++ Models/Cancion.cs
@@ -0,0 +1,9 @@
+﻿namespace LAB14_Ejer2Repaso.Models
+{
+    public class Cancion
+    {
+        public string NombreCancion { get; set; } = string.Empty;
+        public string Artista { get; set; } = string.Empty;
+        public double TiempoDuracion { get; set; }
+    }
+}
